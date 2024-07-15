7ded74ac1c828b41e4d7f70ae5fb7a70e732e0ab
Andrea Marino <am.marinoandrea@gmail.com>
2024-07-15 15:04:17 +0200

    feat(services/policy): moved to postgres database

--- services/policy-manager/policy_manager/config.py
+++ services/policy-manager/policy_manager/config.py
@@ -12,7 +12,5 @@ class Config:
 config = Config(
     env=os.environ.get("ENV", "development"),
     nip_client_address=os.environ.get("NIP_CLIENT_ADDRESS", "localhost:27002"),
-    database_uri=os.environ.get(
-        "POLICY_MANAGER_DATABASE_URI",
-        "sqlite:///~/.local/share/hoppipolla/policy-manager/sqlite.db"),
+    database_uri=os.environ.get("DATABASE_URI", "postgres://localhost:5432/postgres")
 )
