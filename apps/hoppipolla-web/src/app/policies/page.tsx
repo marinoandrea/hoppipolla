465f7f144c4de2f9cea621c6ba073f15f43705f6
Andrea Marino <a.marino2@uva.nl>
2025-01-30 22:15:17 +0100

    chore: removed outdated web frontend application

--- apps/hoppipolla-web/src/app/policies/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Policies() {
-  return redirect("/policies/new");
-}
--- apps/hoppipolla-web/src/services/index.ts
+++
@@ -1,8 +0,0 @@
-import { config } from "@/config";
-
-import { GrpcPolicyManager } from "./policy-manager/grpc";
-import { IPolicyManager } from "./policy-manager/service";
-
-export const policyManager: IPolicyManager = new GrpcPolicyManager(
-  config.POLICY_MANAGER_URI
-);
