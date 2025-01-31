b80c44fb745fcbdd1dbaf983a5a32cf8fed0ba87
Andrea Marino <a.marino2@uva.nl>
2025-01-31 17:39:02 +0100

    perf(services/nip-proxy): parallelization and optional geolocation

--- services/nip-proxy/sources/scion.go
+++ services/nip-proxy/sources/scion.go
@@ -3,19 +3,22 @@ package sources
 import (
 	"context"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/hashicorp/golang-lru/v2/expirable"
 	pb "github.com/marinoandrea/hoppipolla/pkg/proto/nip_proxy/v1"
 	"github.com/scionproto/scion/pkg/addr"
 	"github.com/scionproto/scion/pkg/daemon"
+	"github.com/scionproto/scion/pkg/snet"
 	"googlemaps.github.io/maps"
 )
 
 type ScionNipSourceConfig struct {
-	SciondAddress    string
-	GoogleMapsApiKey string
-	CacheSize        int
+	SciondAddress     string
+	GoogleMapsApiKey  string
+	CacheSize         int
+	EnableGeolocation bool
 }
 
 type ScionCacheKey struct {
@@ -48,6 +51,7 @@ func NewScionNipSource(cfg ScionNipSourceConfig) *ScionNipSource {
 		cacheRefreshChan:   make(chan bool),
 	}
 }
+
 func (s *ScionNipSource) Init(ctx context.Context) error {
 	dconn, err := s.daemon.Connect(ctx)
 	if err != nil {
@@ -56,12 +60,14 @@ func (s *ScionNipSource) Init(ctx context.Context) error {
 	s.conn = dconn
 	log.Printf("Connected to SCION daemon at '%s'", s.config.SciondAddress)
 
-	gclient, err := maps.NewClient(maps.WithAPIKey(s.config.GoogleMapsApiKey))
-	if err != nil {
-		return err
+	if s.config.EnableGeolocation {
+		gclient, err := maps.NewClient(maps.WithAPIKey(s.config.GoogleMapsApiKey))
+		if err != nil {
+			return err
+		}
+		s.googleMapsClient = gclient
+		log.Println("Initialized Google Maps client")
 	}
-	s.googleMapsClient = gclient
-	log.Println("Initialized Google Maps client")
 
 	go func() {
 		for {
@@ -97,7 +103,7 @@ func (s ScionNipSource) Close(ctx context.Context) error {
 }
 
 func (s ScionNipSource) GetMetadata(ctx context.Context, req *pb.GetMetadataRequest) (*Metadata, error) {
-	var out Metadata
+	var out []*Metadata
 
 	src, err := addr.ParseIA(req.Src)
 	if err != nil {
@@ -121,16 +127,36 @@ func (s ScionNipSource) GetMetadata(ctx context.Context, req *pb.GetMetadataRequ
 		return nil, err
 	}
 
-	// TODO(andrea): parallelize
+	wg := sync.WaitGroup{}
+	ch := make(chan Metadata, len(paths))
+
 	for _, path := range paths {
-		for i, current_itf := range path.Metadata().Interfaces {
+		wg.Add(1)
+		go s.getPathMetadata(path, ch, &wg)
+	}
+
+	wg.Wait()
+	close(ch)
+
+	for metadata := range ch {
+		out = append(out, &metadata)
+	}
+
+	return MergeMetadata(out), nil
+}
+
+func (s ScionNipSource) getPathMetadata(path snet.Path, ch chan Metadata, wg *sync.WaitGroup) {
+	var out Metadata
+
+	for i, current_itf := range path.Metadata().Interfaces {
+		if s.config.EnableGeolocation {
 			location := path.Metadata().Geo[i]
-			geocode, err := s.googleMapsClient.ReverseGeocode(ctx, &maps.GeocodingRequest{
+			geocode, err := s.googleMapsClient.ReverseGeocode(context.TODO(), &maps.GeocodingRequest{
 				LatLng: &maps.LatLng{
 					Lat: float64(location.Latitude),
 					Lng: float64(location.Longitude)}})
 			if err != nil {
-				return nil, err
+				log.Println(err)
 			}
 
 			if len(geocode) > 0 {
@@ -150,36 +176,37 @@ func (s ScionNipSource) GetMetadata(ctx context.Context, req *pb.GetMetadataRequ
 					})
 				}
 			}
-
-			if i == len(path.Metadata().Interfaces)-1 {
-				break
-			}
-
-			next_itf := path.Metadata().Interfaces[i+1]
-			link := Link{
-				AsA: current_itf.IA.String(),
-				IfA: current_itf.ID.String(),
-				AsB: next_itf.IA.String(),
-				IfB: next_itf.ID.String(),
-			}
-
-			// FIXME: these should never overflow with realistic data,
-			// however, our reasoner only supports 32-bit integers
-			bandwidth := int32(path.Metadata().Bandwidth[i])
-			latency := int32(path.Metadata().Latency[i])
-
-			out.LinkInfo = append(out.LinkInfo, LinkMetadata{
-				Name:       "bandwidth",
-				Link:       link,
-				ValueInt32: &bandwidth,
-			})
-			out.LinkInfo = append(out.LinkInfo, LinkMetadata{
-				Name:       "latency",
-				Link:       link,
-				ValueInt32: &latency,
-			})
 		}
+
+		if i == len(path.Metadata().Interfaces)-1 {
+			break
+		}
+
+		next_itf := path.Metadata().Interfaces[i+1]
+		link := Link{
+			AsA: current_itf.IA.String(),
+			IfA: current_itf.ID.String(),
+			AsB: next_itf.IA.String(),
+			IfB: next_itf.ID.String(),
+		}
+
+		// FIXME: these should never overflow with realistic data,
+		// however, our reasoner only supports 32-bit integers
+		bandwidth := int32(path.Metadata().Bandwidth[i])
+		latency := int32(path.Metadata().Latency[i])
+
+		out.LinkInfo = append(out.LinkInfo, LinkMetadata{
+			Name:       "bandwidth",
+			Link:       link,
+			ValueInt32: &bandwidth,
+		})
+		out.LinkInfo = append(out.LinkInfo, LinkMetadata{
+			Name:       "latency",
+			Link:       link,
+			ValueInt32: &latency,
+		})
 	}
 
-	return &out, nil
+	ch <- out
+	wg.Done()
 }
